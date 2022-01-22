4327a94c44c021594e8a7b5c5650391403e7d996
SethBurdin <sethburdin@hotmail.com>
2022-01-22 11:28:34 -0500

    Adding auto loop and closing browser at end.

--- main.py
+++ main.py
@@ -1,9 +1,18 @@
 import os
 from time import sleep
+import webbrowser
 from wordgen import randomer
 from setup import search
 
 if __name__ == "__main__":
     sleep(0.5)
-    search()
-    sleep(1)
+    countin = 0
+    while countin < 1:
+        countin =+ 1
+        search(searchTerm=randomer())
+        sleep(15)
+        search(randomer())
+        sleep(5)
+    os.kill(webbrowser.Chrome)
+        
+    
--- wordgen.py
+++ wordgen.py
@@ -5,7 +5,6 @@ import random
 
 with open('dict.txt') as f:
     lines = f.readlines()
-    #lines.translate("\r\n") 
 UpperLimit = len(lines)
 
 
@@ -18,7 +17,7 @@ def randomer():
         randomWord[i] = lines[picker].strip()
         i += 1
     searchTerm = randomWord[0] + " " + randomWord[1] + " " + randomWord[2]
-    #print(searchTerm)    
+        
     return searchTerm
 
 randomer()
