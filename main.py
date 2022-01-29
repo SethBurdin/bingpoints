b2b54d1cd03c75095570b77ab31ced96fb3de709
SethBurdin <sethburdin@hotmail.com>
2022-01-29 11:42:43 -0500

    Bringing up Edge task cleanup. Disarming process.py until feature is needed

--- main.py
+++ main.py
@@ -3,16 +3,15 @@ from time import sleep
 import webbrowser
 from wordgen import randomer
 from setup import search
-from process import taskkill
+
 
 if __name__ == "__main__":
     sleep(0.5)
     countin = 0
     while countin < 25:
-        countin =+ 1
+        countin += 1
         sleep(4)
         search(randomer())
-        sleep(5)
-    taskkill()
+    os.system("taskkill /f /im  msedge.exe")
         
     
--- process.py
+++ process.py
@@ -2,13 +2,14 @@ import psutil
 import os
 import time
 import signal
-
-
-
-def taskkill():
+##Pending Feature to enable custom browser selections
+def processID():
     for proc in psutil.process_iter():
         if proc.name() == 'msedge.exe':
-            processID = proc.pid
-            signal.pthread_kill(processID)
-            
-taskkill()    
+            EdgeID = proc.pid
+            return EdgeID
+
+def taskkill(processID):
+   #signal.pthread_kill(processID)
+
+#taskkill(processID)
